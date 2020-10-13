a1ebbbdd72a64db5e4fbb049c55573746a8e5669
Tjarke <Tjarke@web.de>
2020-10-13 14:50:35 +0200

    added extract_closed_days

---
+++ cleaning_functions.py
@@ -0,0 +1,10 @@
+
+#Extract the days the store is closed from the dataset!
+#Input dataframe
+#Output: dataframe with closed days, dataframe with open days
+
+
+
+def extract_closed(df):
+    select = df.loc[:,"Open"] == 0
+    return df.loc[select,:],df.loc[~select,:]
